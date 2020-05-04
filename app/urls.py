b51ad5f5b689d4b766f8e1c007fa12c3e28f2e59
Duc <uet.nguyenduc@gmail.com>
2020-05-04 21:23:10 +0700

    Update

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('hello', hello)
+    path('hello', hello),
+    path('get_all_categories', getAllCategories),
+    path('search_products', searchProducts),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,47 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from .models import *
+from .serializers import *
 
 @api_view(["GET"])
 def hello(request):
     return Response({"message": "Hello"})
+
+@api_view(["GET"])
+def getAllCategories(request):
+    lst = Category.objects.all()
+    serializer = CategorySerializer(lst, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def searchProducts(request):
+    productList = Product.objects.all()
+    queryParams = request.GET
+    productName = queryParams.get('keyword')
+    categoryId = queryParams.get('categoryId')
+    priceRange = queryParams.get('priceRange')
+
+    minPrice, maxPrice = None, None
+    priceTable = {
+        '1' : [None, 10],
+        '2': [10, 15],
+        '3': [15, 20],
+        '4': [20, None]
+    }
+    if priceRange:
+        minPrice, maxPrice = priceTable.get(priceRange)
+
+    if productName:
+        productList = productList.filter(name__contains=productName)
+    
+    if categoryId:
+        productList = productList.filter(category__id=int(categoryId))
+
+    if minPrice:
+        productList = productList.filter(price__gte=minPrice*1e6)
+
+    if maxPrice:
+        productList = productList.filter(price__lte=maxPrice*1e6)
+
+    serializer = ProductSerializer(productList, many=True)
+    return Response(serializer.data)
